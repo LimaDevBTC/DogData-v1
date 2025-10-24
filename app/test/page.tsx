02b66ccb7f135f6082f3f3c2597c868ab8b67911
LimaDevBTC <limadevbtc@proton.me>
2025-10-24 17:19:15 -0300

    test: add simple test page

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: '20px', color: 'white', background: 'black', minHeight: '100vh' }}>
+      <h1>Test Page - It Works!</h1>
+      <p>If you see this, the deployment is working.</p>
+    </div>
+  )
+}
+
